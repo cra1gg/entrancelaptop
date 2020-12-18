a35afe88656d93b67988ba8c89167dfd6831d3af
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2020-12-18 00:17:53 -0500

    Add initial code for sleep and wake

---
+++ server.py
@@ -0,0 +1,21 @@
+import flask
+import os
+import pyautogui
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/sleep', methods=['GET'])
+def sleep():
+    os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+    return "<h1>Command run sucessfully!</h1>"
+
+@app.route('/wake', methods=['GET'])
+def keypress():
+    pyautogui.press('shift')
+    pyautogui.press('shift')
+    pyautogui.press('shift')
+    return "<h1>Command run sucessfully!</h1>"
+
+app.run(host='0.0.0.0', port=8080)
